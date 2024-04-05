867060635c1bd8ffa6d803e283c1eb06e3009417
Karol Michalik <bel1nqone@gmail.com>
2024-04-05 14:26:58 +0200

    update
    add styling to the site,
    add comments what each line of code do in app.vue

--- shopping-list-1/src/App.vue
+++ shopping-list-1/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
-    <input v-model="input" type="text" />
+    <h1>Your Shopping list</h1>
+    <input v-model="input" type="text" placeholder="What you want to add" />
     <button @click="addItem()">Add to list</button>
 
-    <h2>Items:</h2>
+    <h2>Items to buy:</h2>
     <ul>
       <li v-for="item in validItems" :key="`item-${item.id}`">
         <span @click="deleteItem(item)" style="margin-right: 15px">X</span>
@@ -24,6 +25,7 @@
 export default {
   data() {
     return {
+      //define the input and list array
       input: "",
       list: [],
     };
@@ -32,38 +34,33 @@ export default {
   methods: {
     //method for add item to li
     addItem() {
-      //ush the value to the array
-      this.list.push({
-        id: this.list.length + 1,
-        text: this.input,
-        is_deleted: false,
-      });
+      //push the value to the array if the input have something in it
+      if (this.input.length > 0) {
+        this.list.push({
+          id: this.list.length + 1,
+          text: this.input,
+          is_deleted: false,
+        });
+      }
       //reset the input field
       this.input = "";
     },
-    //for deleting item in list and array
+    //method for deleting item in list and array
+    // pass the current items of array list to the function and after deleting change the boolean value to true
     deleteItem(item) {
       item.is_deleted = true;
     },
   },
 
   computed: {
+    //filter method for filter the items in array if they have a false value in is_deleted
     validItems() {
       return this.list.filter((item) => !item.is_deleted);
     },
-
+    //filter method for filter the items in array if they have a true (was deleted) value in is_deleted
     deletedItems() {
       return this.list.filter((item) => item.is_deleted);
     },
   },
 };
 </script>
-
-<style scoped>
-.container {
-  border: 1px solid black;
-  max-width: 900px;
-  margin: 0 auto;
-  text-align: center;
-}
-</style>
